c007cefdd75ee254210b84c727f84461dbc345de
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-07-27 22:20:27 +0530

    a

--- Java programs/Main.java
+++ Java programs/Main.java
@@ -1,10 +1,35 @@
+/* 
+1.package com.company :
+    Packages are used to group the related classes.
+    The "Package" keyword is used to create packages in Java.
+    Here, com.company is the name of our package.
+2.public class Main :
+    In Java, every program must contain a class.
+    The filename and name of the class should be the same.
+    Here, we've created a class named "Main".
+    It is the entry point to the application.
+3.public static void main(String[]args){..} :
+    This is the main() method of our Java program.
+    Every Java program must contain the main() method.
+4.System.out.println("Hello World"):
+    The above code is used to display the output on the screen.
+    Anything passed inside the inverted commas is printed on the screen as plain text.
+*/
+
+// multiline comment: start (/*) and end (*/)
+
 
 public class Main{
 
     public static void main(String[] args) {
-    // write code here
+    // write your code here
     System.out.println("Hello world");
     
     }
 
 }
+
+
+// add two number
+// AddTwoNumber --> pascal naming convention (for classes)
+// addTwoNumber --> camelCase naming convention (for functions)
