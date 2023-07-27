2014ad924111ce60cddb962691ded8958be7e537
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-07-27 23:14:08 +0530

    a

---
+++ Java programs/sum.java
@@ -0,0 +1,11 @@
+public class sum {
+    public static void main(String[] args) {
+        System.out.print("Sum of three Numbers: ");   // "print" (to print in single line) & "println" (to print and also give an extra line)
+        int num1 = 6;
+        int num2 = 8;
+        int num3 = 10;
+
+        int sum = num1 + num2 + num3;
+        System.out.println(sum);
+    }
+}
