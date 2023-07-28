60056b30803efecb3866166d9bafc7b6a100076c
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-07-28 21:37:06 +0530

    a

---
+++ Java programs/literals.java
@@ -0,0 +1,15 @@
+public class literals {
+    public static void main(String[] args) {
+        byte age = 34;
+        int age2 = 56;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d1 = 5.6;
+        
+        System.out.println(age);
+        System.out.println(age2);
+        System.out.println(ch);
+        System.out.println(f1);
+        System.out.println(d1);
+    }
+}
