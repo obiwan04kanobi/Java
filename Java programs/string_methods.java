f114d3f6000906b1cf46af25d2db64f5f8793318
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-07-30 00:03:20 +0530

    a

--- Java programs/string_methods.java
+++ Java programs/string_methods.java
@@ -1,3 +1,5 @@
+// SOME STRING METHODS
+
 public class string_methods {
     public static void main(String[] args) {
 
@@ -5,7 +7,18 @@ public class string_methods {
         String name = "Mayank";
         String name_1 = new String("Mayank");
 
-        int value = name.length();
-        System.out.println(value); // prints the length of the string.
+        int str_len = name.length();
+        String str_upper = name.toUpperCase();
+        String str_lower = name.toLowerCase();
+
+        String name_2 = "       Mayank          ";
+        String str_trs = name_2.trim();
+
+        System.out.println(str_len); // prints the length of the string.
+        System.out.println(str_upper); // prints the string in uppercase
+        System.out.println(str_lower); // prints the string in lowercase
+        System.out.println(name_2); // untrimmed string with whitespaces
+        System.out.println(str_trs); // trims out all the whitespaces leading to or after the string
+
     }
 }
