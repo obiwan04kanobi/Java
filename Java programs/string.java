a888ece122b5cff4bd35a6b560c5f27491a8d9e1
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-07-29 23:41:21 +0530

    a

---
+++ Java programs/string.java
@@ -0,0 +1,7 @@
+public class string {
+    public static void main(String[] args) {
+        int a = 6;
+        float b = 8.9f;
+        System.out.printf("The value of a is %d and of b is %f",a,b); // Since, you have done C programming you know that "%d" and "%f" are place holders or format specifiers and are used for intger and floats respectively.
+    }
+}
