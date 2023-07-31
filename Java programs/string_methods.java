57d7f7e2bbae83bb050a96702004bb13ab79c1ee
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-07-31 23:14:34 +0530

    a

--- Java programs/string_methods.java
+++ Java programs/string_methods.java
@@ -20,5 +20,31 @@ public class string_methods {
         System.out.println(name_2); // untrimmed string with whitespaces
         System.out.println(str_trs); // trims out all the whitespaces leading to or after the string
 
+        System.out.println(name.substring(3)); // print the string starting from index 3 till the end.
+        System.out.println(name.substring(0)); // print the string starting from index 0 till last, which is essentially printing the whole string as it is.
+
+
+        System.out.println(name.substring(1,5));
+
+        System.out.println(name.replace('a', 'p'));
+        System.out.println(name.replace("ay", "ier"));
+
+        System.out.println(name.startsWith("May"));
+        System.out.println(name.endsWith("dd"));
+
+        System.out.println(name.charAt(4));
+
+        String name_3 = "Harryrryrry";
+        System.out.println(name_3.indexOf("rry")); 
+        System.out.println(name_3.indexOf("rry", 4)); // start searching for string "rry" from 4th index
+        System.out.println(name_3.lastIndexOf("rry")); // gives the last occurance of string "rry"
+        System.out.println(name_3.lastIndexOf("rry", 7)); // start searching for the last occurance of string "rry" before the 7th index. 
+        System.out.println(name_3.indexOf("rry6")); // the output is (-1) when a substring does not exist inside a string. 
+
+        System.out.println(name_3.equals("Harry")); // to check if a given string is equal to a provided string. (NOTE: use this whenever you need to find that if two string are equal rather than using the "==" operator)
+        System.out.println(name_3.equalsIgnoreCase("harry")); // returns true if the strings are equal ignoring the cases (i.e does not matter uppercase or lowercase, as long as the strings are equal).
+
+        System.out.println("I am escape sequence\tdouble quote");
+
     }
 }
