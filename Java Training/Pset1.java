a74f260a4de9e76c36908206784faaf761491b0b
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-08-22 06:59:36 +0000

    a

---
+++ Java Training/Pset1.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.datatypes;
+
+import java.util.Scanner;
+
+public class Pset1 
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter int: ");
+        int n = sc.nextInt();
+        System.out.println((char)n);
+}
