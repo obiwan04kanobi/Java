bc5364825cc1813ab3f5659289fce81343110586
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-08-22 23:23:00 +0530

    a

---
+++ Java Training/char_input.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class char_input {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Name: ");
+        char name = sc.next().charAt(0);
+        System.out.print("hello, ");
+        System.out.println(name);
+
+    }
+}
