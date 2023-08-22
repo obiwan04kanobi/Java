6d928d2f05c4d32691ba40386db5ab1cf4f075c7
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-08-22 23:12:40 +0530

    a

---
+++ Java Training/ascii_code_to_alphabets.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class ascii_code_to_alphabets {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter int: ");
+        int n = sc.nextInt();
+        System.out.println((char)n);
+    }
+}
