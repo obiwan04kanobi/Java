56d083e64257318e39c5c383683ee3da0bc7c01e
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-08-22 17:02:13 +0000

    a

--- Java Training/Pset1.java
+++ Java Training/Pset1.java
@@ -5,9 +5,11 @@ import java.util.Scanner;
 
 public class Pset1 
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter int: ");
         int n = sc.nextInt();
         System.out.println((char)n);
+    }
 }
