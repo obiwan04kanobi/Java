15f3ee04482fe1be30e3a6f9575594bcb9603cfe
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-08-23 12:19:18 +0530

    a

---
+++ Java Workshop/bitwise_operators.java
@@ -0,0 +1,20 @@
+public class bitwise_operators {
+    public static void main(String[] args) {
+        
+        int a = 6;
+        int b = 1;
+
+        System.out.println("OR");
+        System.out.println(a | b);
+
+        System.out.println("AND");
+        System.out.println(a & b);
+
+        System.out.println("XOR");
+        System.out.println(a ^ b);
+
+        System.out.println("Compliment");
+        System.out.println(~a);
+        System.out.println(~b);
+    }
+}
--- Java Workshop/bitwise_shifiting.java
+++ Java Workshop/bitwise_shifiting.java
@@ -1,5 +1,6 @@
 public class bitwise_shifiting {
     public static void main(String[] args) {
+        
         int a = 6;
         int b = 1;
         int c = 2;
