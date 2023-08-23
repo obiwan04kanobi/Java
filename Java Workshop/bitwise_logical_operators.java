8aefeaf4e054b0a1371bb86f39717c816760c5ba
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-08-23 12:24:21 +0530

    a

---
+++ Java Workshop/bitwise_logical_operators.java
@@ -0,0 +1,11 @@
+public class bitwise_logical_operators {
+    public static void main(String[] args) {
+        boolean x = true;
+        boolean y = false;
+
+        System.out.println(x&&y);
+        System.out.println(x||y);
+        System.out.println(!(x&&y));
+        System.out.println(x^y);
+    }
+}
