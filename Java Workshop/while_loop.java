5a7e09078124b6950a3692cbdd58da3153afc9bb
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-08-24 12:12:10 +0530

    a

---
+++ Java Workshop/while_loop.java
@@ -0,0 +1,9 @@
+public class while_loop {
+    public static void main(String[] args) {
+        int a = 4;
+        while (a<10){
+            System.out.println("hello");
+            a += 1;
+        }
+    }
+}
