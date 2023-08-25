bc4a177eff88ffd140cd97396593753d2ecccd70
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-08-25 09:59:06 +0530

    a

---
+++ Java Workshop/nested_for_loop2.java
@@ -0,0 +1,13 @@
+public class nested_for_loop2 {
+    public static void main(String[] args) {
+        for(int i=1; i <=3;i++){
+            
+            if(i==2){
+                continue;
+            }
+            else{
+                System.out.println("Hello Listen !!!");
+            }
+        }
+    }
+}
