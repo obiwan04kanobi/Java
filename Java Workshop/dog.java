f55697d15e63461c143bc089bafb5ab0394be18b
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-08-25 10:52:26 +0530

    a

---
+++ Java Workshop/dog.java
@@ -0,0 +1,23 @@
+public class dog {
+
+    String breed;
+    String size;
+    int age;
+    String color;
+
+    public static void main(String[] args) {
+        // instance variable
+
+        dog maltese = new dog();
+        maltese.breed = "Maltese";
+        maltese.size = "Small";
+        maltese.age = 2;
+        maltese.color = "White";
+        System.out.println(maltese.getinfo());
+    }
+
+    // method 1
+    public String getinfo(){
+        return("Breed is: "+breed+" \nSize is: "+size+" \nAge is: "+age+" \ncolor is: "+color);
+    } 
+}
