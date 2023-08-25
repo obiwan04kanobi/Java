13dd47d37794fb4b58cda1c31364572982cfc258
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-08-25 11:25:52 +0530

    a

---
+++ Java Workshop/print.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class print {
+    String name;
+
+    public void diplay(){
+        Scanner sc = new Scanner(System.in);
+        name = sc.nextLine();
+        System.out.println("Name: "+name);
+    }
+    public static void main(String[] args) {
+        print p = new print();
+        p.diplay();
+    }
+}
