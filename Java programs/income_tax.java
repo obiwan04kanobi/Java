2c9b88d0ec5091033a774bf36110fdace8cc35cb
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-08-25 09:20:28 +0530

    a

--- Java programs/income_tax.java
+++ Java programs/income_tax.java
@@ -2,21 +2,21 @@ import java.util.Scanner;
 
 public class income_tax {
     public static void main(String[] args) {
-        Scanner sc = new Sacanner(System.in);
-        System.out.println("Enter your Salary: ")
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your Salary: ");
         int salary = sc.nextInt();
         
         if (salary < 250000){
-            System.out.println("No TAX to be paid")
+            System.out.println("No TAX to be paid");
         }
         else if ( salary > 250000 & salary < 500000){
-            System.out.println("TAX to be paid: "+(salary-250000)*(5/100))
+            System.out.println("TAX to be paid: "+((salary-250000)*0.05));
         }
         else if ( salary > 500000 & salary < 1000000){
-            System.out.println("TAX to be paid: "+(salary-250000)*(20/100))
+            System.out.println("TAX to be paid: "+((salary-250000)*0.2));
         }
         else if ( salary > 1000000){
-            System.out.println("TAX to be paid: "+(salary-250000)*(30/100))
+            System.out.println("TAX to be paid: "+((salary-250000)*0.3));
         }
     }
 }
