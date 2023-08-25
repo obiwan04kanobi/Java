b1ff5f6323355fbb6e43e347d9858a6366c5a956
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-08-25 12:07:28 +0530

    a

--- Java Workshop/day4/square_root.java
+++ Java Workshop/day4/square_root.java
@@ -1,7 +1,7 @@
 package day4;
 import  java.lang.*;
 
-public class temp {
+public class square_root {
     public static void main(String[] args) {
         System.out.println("Square root of 4 is: "+ Math.sqrt(4));
     }
