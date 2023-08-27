e8d42ef367771a96abfe7ca0de9eaeb14dc4c6bf
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-08-27 22:55:54 +0530

    a

---
+++ Java programs/OOPS/calculator_1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class calculator_1 {
+
+
+    //SUM METHOD/FUNCTION
+    public void sum(int a,int b){
+        int sum = a+b;
+        System.out.printf("%d + %d = %d",a,b,sum);
+    }
+
+    //MAIN CLASS
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Int A: ");
+        int a = sc.nextInt();
+
+
+        System.out.println("Int B: ");
+        int b = sc.nextInt();
+
+        calculator_1 s = new calculator_1(); // CREATING AN OBJECT OF CLASS 
+        s.sum(a,b); // CALLING METHOD SUM USING THE CREATED OBJECT
+    }
+}
