974394382beaa0b9d34675209c8b92f8cb7a2208
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-08-27 13:58:31 +0530

    a

--- Java programs/practice_ch5.java
+++ Java programs/practice_ch5.java
@@ -37,7 +37,7 @@ public class practice_ch5{
 */
 
 
-
+/* 
     //Ques 3.
 
         System.out.println("Enter N: ");
@@ -51,5 +51,21 @@ public class practice_ch5{
         }
     }
 
+
+}
+*/
+
+    // Ques. 4
+
+        System.out.println("Enter N: ");
+
+        int n = sc.nextInt();
+        int mul = n;
+        for(int i = (n-1);i >= 0;i--){
+
+            System.out.println(n+" x "+i+" = "+ mul );
+            mul = (i-1) * n;
+        }
+    }    
 
 }
