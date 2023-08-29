5589a6b26f53748f05c8a238d470bec039e0a2d6
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-08-29 10:47:24 +0530

    a

---
+++ Java Workshop/day7/multi_level_inheritance.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class ParentClass1{
+
+    void input(){
+        Scanner sc = new Scanner(System.in);
+        
+
+
+
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+public class multi_level_inheritance {
+    public static void main(String[] args) {
+        
+    }
+}
