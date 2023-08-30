74e76da26f79a9196ac25de8d34f85a2f99ca8e2
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-08-30 11:07:39 +0530

    a

--- Java Workshop/day8/static_binding.java
+++ Java Workshop/day8/static_binding.java
@@ -2,12 +2,13 @@ package day8;
 
 
     class static_binding{
-
+        
+        //private keyword means that this is a private method which means that to access this method we need to declare in the same class in which we want to access it.
         private void eat(){
             System.out.println("Dog is eating");
         
         }
-        
+
         public static void main(String[] args) {
             static_binding dog = new static_binding();
             dog.eat();
