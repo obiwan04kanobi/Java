064630e6f696cc520075455bc1e1cdb3f032adf9
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-09-04 12:08:54 +0530

    A

--- Java_Workshop/day11/Interface.java
+++ Java_Workshop/day11/Interface.java
@@ -1,6 +1,7 @@
 package day11;
 
 //INTERFACE IS USED TO IMPLEMENT MULTIPLE INHERITANCE WHICH IS NOT SUPPORTED IN JAVA.
+// INTERFACES USES ABSTRACT CLASSES TO IMPLEMENT MULTIPLE INHERITANCE.
 
 interface Pyramid{
 
@@ -16,6 +17,9 @@ class mayank implements Pyramid,Trainer{
 
     public void show(){
         System.out.println("It's working fine");
+    }    
+    public void show1(){
+        System.out.println("We can also put our own child class method");
     }
 }
 
@@ -23,5 +27,6 @@ public class Interface {
     public static void main(String[] args) {
         mayank A = new mayank();
         A.show();
+        A.show1();
     }
 }
