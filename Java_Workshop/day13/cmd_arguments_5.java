f0b5d9f009dfadc852755b490c4d28dc942a4b17
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-09-06 12:13:33 +0530

    a

--- Java_Workshop/day13/cmd_arguments_5.java
+++ Java_Workshop/day13/cmd_arguments_5.java
@@ -5,21 +5,23 @@ package day13;
 public class cmd_arguments_5 {
     public static void main(String[] args) {
 
-        if (args.length == 0) {
-            return;
+        try{
+            if (args.length == 0) {
+                throw new IndexOutOfBoundsException("Enter alteast 1 value");
+            }
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
         }
 
         for (int i = 0; i < args.length; i++) {
             int num = Integer.parseInt(args[i]);
             if (i % 2 == 0) {
-                System.out.print("+" + num);
+                System.out.println("+" + num);
             } else {
-                System.out.print("-" + num);
+                System.out.println("-" + num);
             }
 
-            if (i < args.length - 1) {
-                System.out.print(" ");
-            }
         }
     }
 }
