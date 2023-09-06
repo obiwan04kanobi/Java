54e06374b01dc71557a5f8e2cbf08b2f76dea121
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-09-06 10:10:44 +0530

    a

---
+++ Java_Workshop/day13/exception_6.java
@@ -0,0 +1,31 @@
+package day13;
+
+import java.util.Scanner;
+
+public class exception_6 {
+
+    public static void exception(int num){
+
+        try{
+            if (num == 0){
+                throw new ArithmeticException("Value cannot be zero");
+            }
+            else{
+                System.out.println(num*num);
+            }
+        }
+        catch(Exception e){
+
+            System.out.println(e);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Num: ");
+        int num = sc.nextInt();
+        exception(num);
+
+    }
+}
