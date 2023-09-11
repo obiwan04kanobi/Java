0009ba7e66cd45e5f112419d87b70c57a0ad8c1a
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-09-11 12:17:01 +0530

    a

---
+++ Java_Workshop/day14/static_inner.java
@@ -0,0 +1,23 @@
+package day14;
+
+public class static_inner {
+
+    //static property
+    static int data = 30;
+
+    //static class
+    static class Inner_5{
+
+        //non-static method
+        void mag(){
+            System.out.println("Data is "+data);
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        static_inner.Inner_5 obj = new static_inner.Inner_5();
+        obj.mag();
+    }
+}
