03668ecf3cc1b847798e3f4d90bf641249f75cde
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-09-11 10:03:34 +0530

    a

---
+++ Java_Workshop/day14/access_modifiers.java
@@ -0,0 +1,30 @@
+package day14;
+
+class access{
+    public int w = 10;
+    int x = 45;
+    protected int y = 86;
+    private int z = 123;
+
+    //can be accessed because in same class
+    public void meth(){
+        System.out.println(w);
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+    }
+
+}
+
+public class access_modifiers{
+    public static void main(String[] args) {
+        access ac = new access();
+        ac.meth(); //works
+
+        System.out.println(ac.w);
+        System.out.println(ac.x);
+        System.out.println(ac.y);
+        //System.out.println(ac.z); //not works because property is private
+    }
+   
+}
