8bd3681eb9d4d2879472ffebc336ba9f0a2abc1e
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-09-12 12:07:17 +0530

    a

---
+++ Java_Workshop/day15/lambda_3.java
@@ -0,0 +1,36 @@
+package day15;
+
+import java.util.Scanner;
+
+/*
+ * write a java code to find maximum b/w two numbers using lambda expression
+*/
+
+interface max{
+    int maximum(int x,int y);
+}
+
+public class lambda_3 {
+    public static void main(String[] args) {
+
+        max max = (int x,int y) ->
+        {   
+            if(x>y){
+                return x;
+            }
+            else{
+                return y;
+            }
+        };
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter X: ");
+        int x = sc.nextInt();
+
+        System.out.print("Enter Y: ");
+        int y = sc.nextInt();
+
+        System.out.println(max.maximum(x, y));
+    }
+}
