bdf19133321b7e4cbe69feadbf3f2b373025f4c8
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-09-13 11:53:04 +0530

    a

---
+++ Java_Workshop/day16/file_buffer_1.java
@@ -0,0 +1,17 @@
+package day16;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class file_buffer_1 {
+    public static void main(String[] args) throws IOException {
+        FileOutputStream fout = new FileOutputStream("D:\\CSDS213\\Java\\Java_Workshop\\buffer.txt");
+        BufferedOutputStream bout = new BufferedOutputStream(fout);
+        String s = "Welcome to Buffer Files Concept";
+        byte b[] = s.getBytes();
+        bout.write(b);
+        bout.close();
+        fout.close();
+    }
+}
---
+++ Java_Workshop/day16/file_buffer_2.java
@@ -0,0 +1,21 @@
+package day16;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class file_buffer_2 {
+    public static void main(String[] args) throws IOException{
+        try{        
+            FileInputStream fin = new FileInputStream("D:\\CSDS213\\Java\\Java_Workshop\\buffer.txt");
+            BufferedInputStream bin = new BufferedInputStream(fin);
+            int i = 0;
+            while((i = bin.read()) != -1){
+                System.out.print((char)i);
+            }
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
