299c7a849919dc7da63094069360340b65bd20b4
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-09-13 12:11:13 +0530

    a

---
+++ Java_Workshop/day16/file_dataOuput_1.java
@@ -0,0 +1,15 @@
+package day16;
+
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class file_dataOuput_1 {
+    public static void main(String[] args) throws IOException{
+        DataOutputStream dout = new DataOutputStream(new FileOutputStream("D:\\CSDS213\\Java\\Java_Workshop\\data_output.txt"));
+        dout.writeInt(10);
+        dout.writeChar('a');
+        dout.writeDouble(10.2);
+        dout.close();
+    }
+}
---
+++ Java_Workshop/day16/file_dataOuput_2.java
@@ -0,0 +1,16 @@
+package day16;
+
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class file_dataOuput_2 {
+    public static void main(String[] args) throws IOException{
+        DataInputStream din = new DataInputStream(new FileInputStream("D:\\CSDS213\\Java\\Java_Workshop\\data_output.txt"));
+        int a = din.readInt();
+        char ch = din.readChar();
+        double d = din.readDouble();
+        System.out.println(a+" "+ch+" "+d);
+        din.close();
+    }
+}
