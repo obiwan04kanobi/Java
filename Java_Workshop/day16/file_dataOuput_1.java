4083e34a7a7d68e5af85d10ee79e9405a4c91f3f
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-09-14 09:36:36 +0530

    a

--- Java_Workshop/day16/file_dataOuput_1.java
+++ Java_Workshop/day16/file_dataOuput_1.java
@@ -6,6 +6,9 @@ import java.io.IOException;
 
 public class file_dataOuput_1 {
     public static void main(String[] args) throws IOException{
+
+        //DataOutputStream encrypts the content of the file when it outputs it except string and characters, they are printed as it is  
+
         DataOutputStream dout = new DataOutputStream(new FileOutputStream("D:\\CSDS213\\Java\\Java_Workshop\\day16\\data_output.txt"));
         dout.writeInt(10);
         dout.writeChar('a');
