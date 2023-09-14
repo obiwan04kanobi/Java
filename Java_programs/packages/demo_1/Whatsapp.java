520b0e332245c1bfbb962db48a097779c7da4b33
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-09-14 23:30:11 +0530

    a

---
+++ Java_programs/packages/demo_1/Whatsapp.java
@@ -0,0 +1,12 @@
+package Java_programs.packages.demo_1;
+
+public class Whatsapp {
+    
+    public void status(){
+        System.out.println("Good morning");        
+    }
+
+    public String notification(){
+        return "You received 20 new messages";
+    }
+}
