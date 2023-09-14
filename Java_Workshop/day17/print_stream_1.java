bbe55458a8d34c99eec402b9dfccb01a2632a7c8
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-09-14 09:56:16 +0530

    a

---
+++ Java_Workshop/day17/print_stream_1.java
@@ -0,0 +1,19 @@
+package day17;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+public class print_stream_1 {
+    public static void main(String[] args) throws IOException {
+        FileOutputStream fout = new FileOutputStream("D:\\CSDS213\\Java\\Java_Workshop\\day17\\print.txt");     
+        PrintStream ps = new PrintStream(fout);
+
+        ps.println("Paul McCartneyy");
+        ps.println("Flaming Pie");
+        ps.println(2020);
+
+        ps.close();
+        fout.close();   
+    }
+}
