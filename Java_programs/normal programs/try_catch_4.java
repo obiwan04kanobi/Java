a7e99270ff06692be3ba5ee099c260f01ef66af2
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-09-16 01:36:06 +0530

    a

---
+++ Java_programs/normal programs/try_catch_4.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+// overriding toString() and getMessage() methods of Exception class using MyException class
+class MyException extends Exception{
+
+    @Override
+    public String toString(){   // toString() is an in-built method in Java that returns the value given to it in string format. Hence, any object that this method is applied on, will then be returned as a string object.
+        return "I am toString()";
+    }
+
+    @Override
+    public String getMessage(){
+        return "I am getMessage()";
+    }
+}
+
+public class try_catch_4 {
+    public static void main(String[] args) {        
+        int a;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        a = sc.nextInt();
+
+        if(a<99){
+            try
+            {
+                throw new MyException();
+            }
+            catch (Exception e) {
+                System.out.println(e.getMessage());
+                System.out.println(e.toString());
+                System.out.println(e);
+                e.printStackTrace(); //not an error, just a method to print which error has occured in which line
+            }
+        }
+
+    }
+}
