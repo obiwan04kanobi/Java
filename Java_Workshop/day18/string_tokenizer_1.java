0cd76fc7d4b4e8c0b3c9dd4b49d367e3c884915f
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-09-15 10:35:56 +0530

    a

---
+++ Java_Workshop/day18/string_tokenizer_1.java
@@ -0,0 +1,15 @@
+package day18;
+
+import java.util.StringTokenizer;
+
+public class string_tokenizer_1{
+    public static void main(String[] args) {
+        
+        StringTokenizer st = new StringTokenizer("Hello Stduents of Batch-2");
+
+        // using while loop to print tokens because the String has more than one token/word in it.
+        while (st.hasMoreTokens()) {
+            System.out.println(st.nextToken());
+        }
+    }
+} 
