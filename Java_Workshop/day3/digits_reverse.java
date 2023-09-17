407993471f621193fc3f317330ee12df2f97a5a2
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-09-17 19:24:43 +0530

    a

--- Java_Workshop/day3/digits_reverse.java
+++ Java_Workshop/day3/digits_reverse.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 public class digits_reverse {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int n = sc.nextInt();   //128
         int rev = 0;
         while (n != 0) 
         {
-            int rem = n % 10;
-            rev = (rev * 10) + rem;
-            n = n / 10;    
+            int rem = n % 10;   //8 -> 2 -> 1
+            rev = (rev * 10) + rem;   //8 -> 80 + 2 -> 820 + 1
+            n = n / 10;   //128 -> 12 -> 1 -> 0
         }
-        System.out.println(rev);
+        System.out.println(rev);    //821
 
         sc.close();
     }
