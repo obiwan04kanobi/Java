7ce28a56ce2fae6d85e4c48e06c436a642672229
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-09-17 20:54:03 +0530

    a

--- Java_Workshop/day4/bus.java
+++ Java_Workshop/day4/bus.java
@@ -3,14 +3,22 @@ package day4;
 abstract class bus_2 {
     abstract void accelerate();
 }
+
 class volvo extends bus_2{
+    
     void accelerate(){
         System.out.println("Volvo: accelerate");
     }
+
+    void deaccelerate(){
+        System.out.println("Volvo: deaccelerate");
+    }
 }
+
 class bus{
     public static void main(String[] args) {
         bus_2 obj = new volvo(); // Bus object ==> contents of Volvo
         obj.accelerate(); //Call the method
+       //obj.deaccelerate();    --> gives an error because we are creating object with taking reference of bus_2 so we can only implement methods that are inside bus_2
     }
 }
