889720c4ab3eb1ce0a2867389bd80a9a60c5ac7e
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-09-17 17:52:05 +0530

    a

--- Java_programs/OOPS/io_1.java
+++ Java_programs/OOPS/io_1.java
@@ -1,7 +1,8 @@
 import java.io.*;
+import java.util.Scanner;
 
 public class io_1 {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException,FileNotFoundException{
 
         //code to create a new empty file
         File myfile = new File("/Users/mayankpant/Documents/code/GitHub Projects/Java/Java_programs/OOPS/temp.txt");
@@ -9,7 +10,23 @@ public class io_1 {
 
         //code to write to a file
         FileWriter fileWriter = new FileWriter("/Users/mayankpant/Documents/code/GitHub Projects/Java/Java_programs/OOPS/temp.txt");
-        fileWriter.write("hello, George");
+        fileWriter.write("hello, George\nbye, George");
         fileWriter.close();
+
+        //code to read a file
+        File mFile = new File("/Users/mayankpant/Documents/code/GitHub Projects/Java/Java_programs/OOPS/temp.txt");
+        Scanner sc = new Scanner(mFile);
+        while(sc.hasNextLine()){
+           String line = sc.nextLine();
+           System.out.println(line); 
+        }
+        sc.close();
+/* 
+        //code to delete a file
+        File mFile2 = new File("/Users/mayankpant/Documents/code/GitHub Projects/Java/Java_programs/OOPS/temp.txt");
+        if(mFile2.delete()){
+            System.out.println("I have deleted: "+mFile2.getName());
+        }
+ */
     }
 }
