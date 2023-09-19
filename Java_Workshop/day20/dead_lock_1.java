e678d2f1e6b7cae7dfbb25701480f67d20d46914
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-09-19 12:14:08 +0530

    a

---
+++ Java_Workshop/day20/dead_lock_1.java
@@ -0,0 +1,45 @@
+package day20;
+
+public class dead_lock_1 {
+    String s1 = "Nitish";
+    String s2 = "Piyush Gupta";
+    Thread t1 = new Thread(){
+        public void run(){
+            while(true){
+                synchronized(s1){
+                    try {
+                        System.out.println("Hi Nitish");
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        // TODO: handle exception
+                        e.printStackTrace();
+                    }
+                    synchronized(s2){
+                        System.out.println(Thread.currentThread().getName() + " locked "+s2);
+                    }
+                }
+            }
+        }
+    };
+
+    Thread t2 = new Thread(){
+        public void run(){
+            while(true){
+                synchronized(s2){
+
+                System.out.println("Hi Piyush Gupta");
+                }
+                synchronized(s1){
+                    System.out.println(Thread.currentThread().getName() + " locked "+s1);
+                }
+            }
+        }
+    };
+
+    public static void main(String[] args) {
+        dead_lock_1 obj = new dead_lock_1();
+        obj.t1.start();
+        obj.t2.start();
+    }
+
+}
