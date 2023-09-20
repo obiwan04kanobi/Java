78854ae042f644b8b4ecf60e6965c760f3de0d8a
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-09-20 10:34:33 +0530

    a

---
+++ Java_Workshop/day21/annotation_2.java
@@ -0,0 +1,24 @@
+package day21;
+
+class A{
+
+    void msg(){
+        System.out.println("Hello");
+    }
+}
+
+// this piece of code will show show warnings but will execute
+
+/* class B extends A{
+    @Override
+    void message(){
+        System.out.println("Hello Team !");
+    }
+} 
+ */
+public class annotation_2 {
+    public static void main(String[] args) {
+        //B b = new B();
+        //b.msg();
+    }
+}
