eda501f4029e635209c11693448191de7d39372f
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-09-23 23:08:25 +0530

    a

---
+++ Java_programs/normal programs/Reverse_String_2.java
@@ -0,0 +1,16 @@
+public class Reverse_String_2{
+    public static void main(String[] args) {
+
+        char [] s = {'h','e','l','l','o'};
+        int left = 0;
+        int right = s.length - 1;
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+        System.out.println(s);
+    }
+}
