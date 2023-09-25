3730dc737e3b793ccd7259d75e78d0e402866344
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-09-25 20:41:30 +0530

    a

--- Java_programs/normal programs/Pangram.java
+++ Java_programs/normal programs/Pangram.java
@@ -1,16 +1,25 @@
 public class Pangram {
     public static void main(String[] args) {
         String sentence = "thequickbrownfoxjumpsoverthelazydog";
-        char [] ch = sentence.toCharArray();
-        int sum = 0;
-        for (int i = 0; i < ch.length; i++) {
-            sum += (int)ch[i];
+        
+        // Create a boolean array to mark the presence of each letter
+        boolean[] alphabet = new boolean[26];
+        
+        // Iterate through each character in the sentence
+        for (char c : sentence.toCharArray()) {
+            // If the character is a lowercase letter, mark it as present
+            if (c >= 'a' && c <= 'z') {
+                alphabet[c - 'a'] = true;
+            }
         }
-
-        if (sum >= 2847) {
-            System.out.println(true);
-        } else {
-            System.out.println(false);
+        
+        // Check if all letters have been marked as present
+        for (boolean present : alphabet) {
+            if (!present) {
+                System.out.println(true);;
+            }
         }
+        
+        System.out.println(false);
     }
 }
