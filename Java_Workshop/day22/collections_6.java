206ab90e3a4e69d832767ed8e8158999185a48c7
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-09-25 11:41:54 +0530

    a

---
+++ Java_Workshop/day22/collections_6.java
@@ -0,0 +1,14 @@
+package day22;
+import java.util.*;
+public class collections_6 {
+    public static void main(String[] args) {
+        ArrayList<String> as = new ArrayList<String>();
+        as.add("Java");
+        as.add("Python");
+        as.add("C#");
+        System.out.println("Elements"+as);
+        Collections.addAll(as,"HTML","DSA","JS","CSS","RDMS","REACT"); //addAll() method adds all the elements of specified collection to the list
+        System.out.println(as);
+        System.out.println("Index value: "+as.get(4)); //get() method returns the element at the specified position in the list
+    }
+}
