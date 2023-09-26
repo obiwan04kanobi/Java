552859fcc5acb8229a85a8fac3f705b667e30319
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-09-26 10:43:04 +0530

    a

---
+++ Java_Workshop/day23/linked_list_2.java
@@ -0,0 +1,26 @@
+package day23;
+
+import java.util.*;
+
+public class linked_list_2 {
+    public static void main(String[] args) {
+        List<String> ls = new LinkedList<>();
+        ls.add("Nirav Modi");
+        ls.add("Mehul Choksi");
+        ls.add("Lalit Modi");
+        ls.add("Jatin Mehta");
+        ls.add("Vijay Malya");
+        System.out.println(ls);
+        System.out.println("Particular Element: "+ls.get(2));
+        ls.set(2, "Rajeev Gandhi");
+        System.out.println("Updated List: "+ls);
+        ls.remove(3);
+        ls.remove("Mehul Choksi");
+        System.out.println("After Removing: "+ls);
+
+        Collections.addAll(ls, "Preet", "Sanjay", "Rahul", "Preet", "Shagun");
+        System.out.println("After Adding: "+ls);
+        System.out.println("First Occurance of Preet: "+ls.indexOf("Preet"));
+        System.out.println("Last Occurance of Preet: "+ls.lastIndexOf("Preet"));
+    }
+}
