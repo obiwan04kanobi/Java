8b7e759acfa2d73fbc70ba83c391d70a90783c5f
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-09-27 09:36:59 +0530

    a

--- Java_Workshop/day24/vectors_1.java
+++ Java_Workshop/day24/vectors_1.java
@@ -2,7 +2,9 @@ import java.util.*;
 
 public class vectors_1{
     public static void main(String[] args) {
-        Vector<Integer> v1 = new Vector<>();
+        Vector<Integer> v1 = new Vector<>(); // Vector is a class. It is a dynamic array. It is synchronized.
+        v1.add(4);
+        v1.add(78);
         v1.add(13);
         v1.add(27);
         v1.add(67);
@@ -10,6 +12,11 @@ public class vectors_1{
         Vector<Integer> v2 = new Vector<>();
         v2.addAll(v1);
         System.out.println("Copying V1 to V2: "+v2);
+
+        List<Integer> v3 = new ArrayList<>();
+        v3 = v1.subList(2, 4);
+        System.out.println("SubList: "+v3);
+
 
     }
 }
