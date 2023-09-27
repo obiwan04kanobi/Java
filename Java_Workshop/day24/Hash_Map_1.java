f4344af584b3682bc1870d5767e26a20e3169a05
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-09-27 10:39:42 +0530

    a

--- Java_Workshop/day24/Hash_Map_1.java
+++ Java_Workshop/day24/Hash_Map_1.java
@@ -4,7 +4,7 @@ public class Hash_Map_1 {
     public static void main(String[] args) {
         
         Map<String,Integer> hm = new HashMap<>(); // it stores the data in key-value pair. values are unordered and unsorted.
-        hm.put("Rajeev",98);
+        hm.put("Rajeev",98); // put() is used to add the data in the map.
         hm.put("Shagun",89);
         hm.put("Mehul",37);
         hm.put("Kashif",45);
@@ -13,10 +13,16 @@ public class Hash_Map_1 {
         System.out.println("Keys: "+hm.keySet());
         System.out.println("Values: "+hm.values());
         System.out.println("Implementing by using Iterator");
+        
         for (String keys : hm.keySet()) {
             System.out.println(keys+" : "+hm.get(keys));
         }
-            
+        
+        hm.replace("Mehul", 76);    // replace() is used to replace the value of the key.  
+        System.out.println("Updated: "+hm);
+
+        hm.remove("Rajeev");    // remove() is used to remove the key-value pair. 
+        System.out.println("Updated: "+hm);
     }
 }
 
