506f34da22e4060514250ce16a8d1e9ff803bda5
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-09-27 09:37:37 +0530

    a

--- Java_Workshop/day24/vectors_1.java
+++ Java_Workshop/day24/vectors_1.java
@@ -10,11 +10,11 @@ public class vectors_1{
         v1.add(67);
         System.out.println("Vector Values: "+v1);
         Vector<Integer> v2 = new Vector<>();
-        v2.addAll(v1);
+        v2.addAll(v1); // Copying v1 to v2. addAll() is a method of Vector class.
         System.out.println("Copying V1 to V2: "+v2);
 
         List<Integer> v3 = new ArrayList<>();
-        v3 = v1.subList(2, 4);
+        v3 = v1.subList(2, 4); // subList() is a method of Vector class. It returns a list.
         System.out.println("SubList: "+v3);
 
 
