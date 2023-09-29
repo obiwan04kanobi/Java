8522100485c4951d783534228248af1bd99fabd5
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-09-29 11:25:16 +0530

    a

--- Java_Workshop/day25/Singleton.java
+++ Java_Workshop/day25/Singleton.java
@@ -2,18 +2,55 @@ package day25;
 
 public class Singleton {
     public static void main(String[] args) {
-        Kunal obj = Kunal.getInstance();
-        Kunal obj1 = Kunal.getInstance(); // obj and obj1 are same object.
-        //output will be "I think I'am Listening" only once.
+
+        //threads method will take 100 miliseconds each to execute, which is a lot so, it is not recommended.
+        Thread t1 = new Thread(new Runnable(){
+            
+            public void run(){
+                Kunal obj = Kunal.getInstance();
+            }
+        });
+        
+        Thread t2 = new Thread(new Runnable(){
+            
+            public void run(){
+                Kunal obj1 = Kunal.getInstance();
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        //Kunal obj = Kunal.getInstance();
+        //Kunal obj1 = Kunal.getInstance(); // obj and obj1 are same object.
+        //output will be "Instance Created" only once.
     }
 }
-class Kunal{
+
+/* class Kunal{
     
     static Kunal obj = new Kunal(); // Eager Initialization. Early Singleton. invoked at initilization.
     private Kunal(){
-        System.out.println("I think I'am Listening");
+        System.out.println("Instance Created");
     }
     public static Kunal getInstance(){
+
+        return obj;
+    }
+} */
+
+class Kunal{
+
+    static Kunal obj; // Lazy Initialization. Late Singleton. invoked at first call.
+    private Kunal(){
+        System.out.println("Instance Created");
+    }
+    public static Kunal getInstance(){
+         if (obj==null) {
+            obj = new Kunal(); // will display "Instance Created" only once.
+        } 
+        
+        //obj = new Kunal(); // will display "Instance Created" twice.
         return obj;
     }
 }
