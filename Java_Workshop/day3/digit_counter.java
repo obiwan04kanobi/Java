6d47c93ad896eacf33d676234f8a7f877760bc0d
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-10-10 18:26:56 +0530

    a

--- Java_Workshop/day3/digit_counter.java
+++ Java_Workshop/day3/digit_counter.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class digit_counter {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter N: ");
         int n = sc.nextInt();
         int count = 0;
         do{
